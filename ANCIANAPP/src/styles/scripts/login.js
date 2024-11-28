6088970517a05bac6f0d6c94a5ac4ef71bc9ed8c
Luky2908 <luiscarlosquesadajimenez@gmail.com>
2024-11-28 16:10:10 -0500

    first commit

---
+++ ANCIANAPP/src/styles/scripts/login.js
@@ -0,0 +1,4 @@
+document.getElementById("logout").addEventListener("click", function() {
+    alert("Has cerrado sesión.");
+    window.location.href = "index.html"; // Redirige a la página de inicio de sesión
+});
